6ec1e9ad0e6ef79eec8cb2b2e76c54edc13e77e7
Ziyue <76886755+what-is-that-Hmmm@users.noreply.github.com>
2023-05-02 21:03:50 +0800

    Added all completed tasks

--- CallPCA.py
+++ CallPCA.py
@@ -7,8 +7,10 @@ import matplotlib.pyplot as plt
 # Load original dataset
 F_data = pandas.read_csv('data.csv',usecols=["F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12","F13","F14","F15"])
 
+
 # Normalize the data in column manners
 F_Normalized = F_data
+
 for column in F_data.columns:
     F_Normalized[column] = F_Normalized[column]  / F_Normalized[column].abs().max()
 # Print normalized data
@@ -21,12 +23,11 @@ PC_project = PCA(n_components=pc_num)
 PC_project.fit(F_Normalized)
 print(PC_project.explained_variance_ratio_)
 
-# Reformat
+# View results of PCA
 print("components are:")
 print(PC_project.components_.T)
-
-# View results
 plt.plot(PC_project.explained_variance_ratio_, marker='o')
 plt.ylabel('Explained Variance')
 plt.xlabel('Components')
 plt.show()
+
